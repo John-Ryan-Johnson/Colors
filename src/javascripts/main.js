cc78e51d8ebe5b041c5632f73f3a2c604b58d653
John-Ryan-Johnson <john4johnkids@gmail.com>
2019-10-15 11:00:54 -0500

    done

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,3 +1,15 @@
+// import arrayMethods from './arrayMethods';
+import cardList from './components/cardList/cardList';
+
+import c from './helpers/data/colors';
+
 import '../styles/main.scss';
 
-console.error('hi');
+const init = () => {
+  // arrayMethods.init();
+  const colors = c.getColors();
+  cardList.cardListEvents();
+  cardList.cardListBuilder(colors);
+};
+
+init();
