84a01ac8d787d5775b5269345dd1f82252be49f2
John-Ryan-Johnson <john4johnkids@gmail.com>
2019-10-14 20:40:16 -0500

    setup complete

---
+++ src/javascripts/helpers/data/colors.js
@@ -0,0 +1,42 @@
+const colors = [
+  {
+    id: 'color1',
+    name: 'red',
+    hexValue: 'FF0000',
+    upvotes: 15,
+  },
+  {
+    id: 'color2',
+    name: 'orange',
+    hexValue: 'FF7F00',
+    upvotes: 15,
+  },
+  {
+    id: 'color3',
+    name: 'yellow',
+    hexValue: 'FFFF00',
+    upvotes: 15,
+  },
+  {
+    id: 'color4',
+    name: 'green',
+    hexValue: '00FF00',
+    upvotes: 15,
+  },
+  {
+    id: 'color5',
+    name: 'blue',
+    hexValue: '0000FF',
+    upvotes: 15,
+  },
+  {
+    id: 'color6',
+    name: 'violet',
+    hexValue: '9400D3',
+    upvotes: 15,
+  },
+];
+
+const getColors = () => colors;
+
+export default { getColors };
